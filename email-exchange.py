ca3eb5525385b7f67bb85eda264415ab14d897ab
Rojar Smith <rojarsmith@gmail.com>
2024-07-17 16:56:14 +0800

    feat: Support Mailjet

--- email-exchange.py
+++ email-exchange.py
@@ -3,17 +3,21 @@ import win32com.client
 import pythoncom
 import smtplib
 from email.mime.text import MIMEText
-
+import requests
+from mailjet_rest import Client
 
 # Load config
 with open("config.properties", "r+b") as f:
     p = Properties()
     p.load(f, "utf-8")
 
+    exchange_mode = p.properties.get("exchange.mode")
     smtp_server = p.properties.get("smtp.server")
     smtp_port = p.properties.get("smtp.port")
     smtp_user = p.properties.get("smtp.user")
     smtp_password = p.properties.get("smtp.password")
+    mailjet_api_key = p.properties.get("mailjet.api.key")
+    mailjet_api_secret = p.properties.get("mailjet.api.secret")
     recipient = p.properties.get("recipient")
 
 
@@ -37,7 +41,9 @@ class OutlookEvents:
             body = mail.Body
             # print(mail)
             print("Receive:", subject, body)
-            send_notification(subject, body)
+            filters = ["member@digitimes.com"]
+            if not all(f in body for f in filters):
+                send_notification(subject, body)
 
 
 def send_notification(subject, body):
@@ -47,16 +53,45 @@ def send_notification(subject, body):
     mimet["To"] = recipient
     msg = mimet.as_string()
 
-    server = smtplib.SMTP(smtp_server, smtp_port)
-    server.ehlo()
-    server.starttls()
-    server.login(smtp_user, smtp_password)
-    status = server.sendmail(smtp_user, recipient, msg)
-    if status == {}:
-        print("OK")
-    else:
-        print("FAILED")
-    server.quit()
+    if exchange_mode == "smtp":
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.ehlo()
+        server.starttls()
+        server.login(smtp_user, smtp_password)
+        status = server.sendmail(smtp_user, recipient, msg)
+        if status == {}:
+            print("OK")
+        else:
+            print("FAILED")
+        server.quit()
+
+    if exchange_mode == "mailjet":
+        api_key = mailjet_api_key
+        api_secret = mailjet_api_secret
+        mailjet = Client(auth=(api_key, api_secret), version='v3.1')
+        data = {
+            'Messages': [
+                {
+                    "From": {
+                        "Email": smtp_user,
+                        "Name": "Rojar Smith"
+                              },
+          "To": [
+            {
+              "Email": recipient,
+              "Name": "Rojar"
+            }
+            ],
+                    "Subject": subject,
+                    "TextPart": "My first Mailjet email",
+                    "HTMLPart": body,
+                    "CustomID": "AppGettingStartedTest"
+                }
+            ]
+        }
+        result = mailjet.send.create(data=data)
+        print(result.status_code)
+        print(result.json())       
 
 
 if __name__ == "__main__":
